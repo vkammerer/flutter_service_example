1041a7a369895cf58e74ad82643c452532eed22f
Vincent <vkammerer@gmail.com>
2020-07-23 19:14:04 +1200

    Adds first version of the demo - copy of the original example directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,181 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:math';
+import 'dart:ui';
 
-void main() {
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_local_notifications_broadcast/flutter_local_notifications_broadcast.dart';
+import 'package:flutter_service/flutter_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Global [SharedPreferences] object.
+SharedPreferences prefs;
+
+/// The [SharedPreferences] key to access the alarm fire count.
+const String countKey = 'count';
+
+/// A port used to communicate from a background isolate to the UI isolate.
+final ReceivePort port = ReceivePort();
+
+/// The name associated with the UI isolate's [SendPort].
+const String isolateName = 'isolate';
+
+final FlutterLocalNotificationsBroadcast flutterLocalNotificationsBroadcast =
+    FlutterLocalNotificationsBroadcast();
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+NotificationAppLaunchDetails notificationAppLaunchDetails;
+
+var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'your channel id', 'your channel name', 'your channel description',
+    importance: Importance.max, priority: Priority.high, ticker: 'ticker');
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  IsolateNameServer.registerPortWithName(
+    port.sendPort,
+    isolateName,
+  );
+  prefs = await SharedPreferences.getInstance();
+  if (!prefs.containsKey(countKey)) {
+    await prefs.setInt(countKey, 0);
+  }
+
+  notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  var initializationSettings = InitializationSettings(
+      android: AndroidInitializationSettings('app_icon'));
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onSelectNotification: (String payload) async {
+      if (payload != null) {
+        debugPrint('notification payload: ' + payload);
+      }
+    },
+  );
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _MyAppState extends State<MyApp> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  void initialize() async {
+    FlutterService.initialize(jobIntentService: true, foregroundService: true);
+    port.listen((_) async {
+      await prefs.reload();
+      setState(() {
+        _counter = prefs.getInt(countKey);
+      });
+    });
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter = prefs.getInt(countKey);
     });
   }
 
+  static Future<void> updateCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.reload();
+    int currentCount = prefs.getInt(countKey);
+    await prefs.setInt(countKey, currentCount + 1);
+
+    // This will be null if we're running in the background.
+    SendPort uiSendPort = IsolateNameServer.lookupPortByName(isolateName);
+    uiSendPort?.send(null);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text('Shared preferences counter: $_counter'),
+                RaisedButton(
+                  child: Text(
+                    'Update counter',
+                  ),
+                  onPressed: () async {
+                    updateCounter();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Show Notification in ForegroundService'),
+                  onPressed: () async {
+                    await flutterLocalNotificationsBroadcast.broadcast(
+                      1,
+                      'Hello',
+                      'I am the Foreground Service notification',
+                      FlutterService
+                          .foregroundServiceBroadcastReceiverClassName,
+                      notificationDetails: androidPlatformChannelSpecifics,
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text(
+                    'Update counter from JobIntentService via Alarm Manager',
+                  ),
+                  onPressed: () async {
+                    await AndroidAlarmManager.oneShot(
+                      const Duration(seconds: 5),
+                      // Ensure we have a unique alarm ID.
+                      Random().nextInt(pow(2, 31)),
+                      updateCounter,
+                      exact: true,
+                      wakeup: true,
+                      allowWhileIdle: true,
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text(
+                      'Update counter from ForegroundService via Alarm Manager'),
+                  onPressed: () async {
+                    await AndroidAlarmManager.oneShot(
+                      const Duration(seconds: 10),
+                      // Ensure we have a unique alarm ID.
+                      Random().nextInt(pow(2, 31)),
+                      updateCounter,
+                      exact: true,
+                      allowWhileIdle: true,
+                      wakeup: true,
+                      rescheduleOnReboot: true,
+                      serviceType: 'ForegroundService',
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Stop ForegroundService'),
+                  onPressed: () async {
+                    await FlutterService.stopForegroundService();
+                  },
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
